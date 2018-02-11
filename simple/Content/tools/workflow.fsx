846a970b1c5e45078608e03f71465c805c40d7a4
humhei <humhei@outlook.com>
2018-02-11 16:25:50 +0800

    print console with normal color

--- simple/Content/tools/workflow.fsx
+++ simple/Content/tools/workflow.fsx
@@ -1,26 +1,30 @@
+open System
 
 #r "paket:
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target //"
 #load "./.fake/workflow.fsx/intellisense.fsx"
-open Fake.DotNet.Cli
+
 open Fake.IO
 open Fake.Core
-let dir = "./tools/" |> Path.getFullName
-
+let mutable dotnetExePath = "dotnet"
+let toolDir = "./tools/" |> Path.getFullName
+let runDotnet workingDir args =
+    let result =
+        Process.ExecProcess (fun info ->
+        { info with 
+            FileName = dotnetExePath
+            WorkingDirectory = workingDir 
+            Arguments =  args }) TimeSpan.MaxValue
+    if result <> 0 then failwithf "dotnet %s failed" args 
+    
 Target.Create "Build" (fun _ ->
-    Dotnet 
-        {DotnetOptions.Default with DotnetCliPath = "dotnet"; WorkingDirectory = dir} 
-        "fable webpack -- -p --config webpack.config.prod.js"
-    |> ignore   
+    runDotnet toolDir "fable webpack -- -p --config webpack.config.prod.js"
 )
 
 Target.Create "Start" (fun _ ->
-    Dotnet 
-        {DotnetOptions.Default with DotnetCliPath = "dotnet"; WorkingDirectory = dir} 
-        "fable webpack-dev-server -- --config webpack.config.dev.js"
-    |> ignore   
+    runDotnet toolDir "fable webpack-dev-server -- --config webpack.config.dev.js"
 )
 
 Target.RunOrDefault "Start"
